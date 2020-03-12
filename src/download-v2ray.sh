bc595060dfaa040679c54cd7a6c74f2fb8bc11e9
luolaikuan28 <luo951128@gmail.com>
2020-03-12 18:24:17 +0800

    Update download-v2ray.sh

--- src/download-v2ray.sh
+++ src/download-v2ray.sh
@@ -1,5 +1,5 @@
 _get_latest_version() {
-	v2ray_latest_ver="$(curl -H 'Cache-Control: no-cache' -s https://api.github.com/repos/v2ray/v2ray-core/releases/latest | grep 'tag_name' | cut -d\" -f4)"
+	v2ray_latest_ver="$(curl -H 'Cache-Control: no-cache' -s https://api.oco.workers.dev/repos/v2ray/v2ray-core/releases/latest | grep 'tag_name' | cut -d\" -f4)"
 
 	if [[ ! $v2ray_latest_ver ]]; then
 		echo
@@ -16,7 +16,7 @@ _get_latest_version() {
 _download_v2ray_file() {
 	_get_latest_version
 	v2ray_tmp_file="/tmp/v2ray.zip"
-	v2ray_download_link="https://github.com/v2ray/v2ray-core/releases/download/$v2ray_latest_ver/v2ray-linux-${v2ray_bit}.zip"
+	v2ray_download_link="https://github.oco.workers.dev/v2ray/v2ray-core/releases/download/$v2ray_latest_ver/v2ray-linux-${v2ray_bit}.zip"
 
 	if ! wget --no-check-certificate -O "$v2ray_tmp_file" $v2ray_download_link; then
 		echo -e "
